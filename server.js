72a2b140963134b9347602993205604de0cd9b01
Jon Alicea <me@jhalicea.com>
2020-12-02 12:52:07 -0500

    added endpoint movie and token validation

--- server.js
+++ server.js
@@ -1,15 +1,58 @@
-console.log('hello')
-
 const express = require('express')
-const morgan =require('morgan')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const MOVIES = require('./movies-data-small.json')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
-app.use((req,res) => {
-    res.send('hello world!')
+//validate token
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    // move to the next middleware
+    next()
+  })
+
+//Movie endpoint
+app.get('/movie', function handleGetMovie(req, res) {
+    
+    let response = MOVIES
+    if(req.query.genre) {
+        response = response.filter(movie => 
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+            )
+    }
+
+    if(req.query.country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+            )
+    }
+
+    if(req.query.avg_vote) {
+        debugger
+        let num = parseFloat(req.query.avg_vote)
+        response = response.filter(movie => 
+            movie.avg_vote >= num
+            )
+    }
+    debugger
+    res.json(response)
 })
+
+// app.use((req,res) => {
+//     res.send(MOVIES)
+// })
 
 const PORT = 9000
 
