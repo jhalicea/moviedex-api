4b7b375a7dea517e3ac38c946888d3fe1a4b0ae0
Jon Alicea <me@jhalicea.com>
2020-12-02 11:23:30 -0500

    first

---
+++ server.js
@@ -0,0 +1,18 @@
+console.log('hello')
+
+const express = require('express')
+const morgan =require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use((req,res) => {
+    res.send('hello world!')
+})
+
+const PORT = 9000
+
+app.listen(PORT, () => {
+console.log(`Server listening at http://localhost:${PORT}`)
+})
